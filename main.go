36ccd408749b6e9084f0c88549545c26d3e39e19
Will Lehman <william.p.lehman@usace.army.mil>
2022-05-22 07:35:24 -0400

    working through job sorting

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/usace/wat-api/utils"
 	"github.com/usace/wat-api/wat"
 	"gopkg.in/yaml.v2"
 )
@@ -25,5 +29,51 @@ func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 }
 
 func main() {
-	lambda.Start(handler)
+	fmt.Println("initializing a wat-lambda")
+	loader, err := utils.InitLoader("")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	queue, err := loader.InitQueue()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	lambda.Start(func(ctx context.Context, sqsEvent events.SQSEvent) error {
+		for _, message := range sqsEvent.Records {
+			task := wat.Task{} //needs to be a Task from batch branch.
+			err := yaml.Unmarshal([]byte(string(message.Body)), &task)
+			if err != nil {
+				fmt.Println("error while parsing message body", err)
+			}
+			fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+			fmt.Println("sending task to", task.TaskType)
+			if task.TaskType == "Lambda" {
+				fmt.Println("sending message: ", task.ModelPayload)
+				blob, err := yaml.Marshal(task.ModelPayload)
+				payload := string(blob)
+				queueName := "lambda-tasks" //sending to a different sqs queue (probably needs to be by plugin type?)
+				input := sqs.GetQueueUrlInput{
+					QueueName: &queueName,
+				}
+				queueURL, err := queue.GetQueueUrl(&input)
+				if err != nil {
+					return err
+				}
+				fmt.Println("sending message to:", queueURL.QueueUrl)
+				_, err = queue.SendMessage(&sqs.SendMessageInput{
+					DelaySeconds: aws.Int64(1),
+					MessageBody:  aws.String(payload),
+					QueueUrl:     queueURL.QueueUrl,
+				})
+				fmt.Println("message sent")
+			} else {
+				//default to batch.
+
+			}
+		}
+
+		return nil
+	})
 }
